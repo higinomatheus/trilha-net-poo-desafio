c5621fca92f744a088e119e306dc7edcd1318641
Matheus Higino <matheus.higino@yahoo.com.br>
2024-08-22 21:41:15 -0300

    criacao das funcionalidades solicitadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone s1 = new Nokia("123", "XYZ", "145", 64);
+
+Smartphone s2 = new Iphone("456", "XPTO", "147", 64);
+
+s1.InstalarAplicativo("XP Investimentos");
+s2.InstalarAplicativo("XP Investimentos");
